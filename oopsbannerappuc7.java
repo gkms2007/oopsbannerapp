907e718cb843085c116be402811c70fb113ccb88
gkmohanashankar <gs9861@srmist.edu.in>
2026-03-26 09:49:37 +0530

    Add UC7 code

--- oopsbannerappuc7.java
+++ oopsbannerappuc7.java
@@ -1,5 +1,83 @@
-public class oopsbannerappuc7 {
+import java.util.HashMap;
+import java.util.Map;
+
+public class OOPSBannerApp7 {
+
+    // Static Inner Class to encapsulate character and pattern
+    static class CharacterPattern {
+        private char character;
+        private String[] pattern;
+
+        public CharacterPattern(char character, String[] pattern) {
+            this.character = character;
+            this.pattern = pattern;
+        }
+
+        public String[] getPattern() {
+            return pattern;
+        }
+    }
+
+    // Centralized storage
+    private static final Map<Character, CharacterPattern> patternMap = new HashMap<>();
+
+    // Load patterns
+    static void loadPatterns() {
+
+        patternMap.put('O', new CharacterPattern('O', new String[]{
+                "*****",
+                "*   *",
+                "*   *",
+                "*   *",
+                "*****",
+                "     ",
+                "     "
+        }));
+
+        patternMap.put('P', new CharacterPattern('P', new String[]{
+                "*****",
+                "*   *",
+                "*****",
+                "*    ",
+                "*    ",
+                "     ",
+                "     "
+        }));
+
+        patternMap.put('S', new CharacterPattern('S', new String[]{
+                "*****",
+                "*    ",
+                "*****",
+                "    *",
+                "*****",
+                "     ",
+                "     "
+        }));
+    }
+
+    // Dynamic display method
+    static void displayBanner(String word) {
+
+        int height = 7;
+
+        for (int i = 0; i < height; i++) {
+
+            StringBuilder line = new StringBuilder();
+
+            for (char ch : word.toCharArray()) {
+                CharacterPattern cp = patternMap.get(ch);
+                if (cp != null) {
+                    line.append(cp.getPattern()[i]).append("  ");
+                }
+            }
+
+            System.out.println(line);
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("UC7 Implementation");
+
+        loadPatterns();
+        displayBanner("OOPS");
     }
 }

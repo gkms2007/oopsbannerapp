e56cb9a7390feb2511f4296990960f7724bdff2d
gkmohanashankar <gs9861@srmist.edu.in>
2026-03-26 09:47:23 +0530

    Add UC6 code

--- oopsbannerappuc6.java
+++ oopsbannerappuc6.java
@@ -1,5 +1,53 @@
-public class oopsbannerappuc6 {
+public class OOPSBannerApp6 {
+
+    // Helper method for letter O
+    static String[] buildO() {
+        return new String[] {
+            String.join("", "*", "*", "*", "*", "*"),
+            String.join("", "*", " ", " ", " ", "*"),
+            String.join("", "*", " ", " ", " ", "*"),
+            String.join("", "*", " ", " ", " ", "*"),
+            String.join("", "*", "*", "*", "*", "*"),
+            String.join("", " ", " ", " ", " ", " "),
+            String.join("", " ", " ", " ", " ", " ")
+        };
+    }
+
+    // Helper method for letter P
+    static String[] buildP() {
+        return new String[] {
+            String.join("", "*", "*", "*", "*", "*"),
+            String.join("", "*", " ", " ", " ", "*"),
+            String.join("", "*", "*", "*", "*", "*"),
+            String.join("", "*", " ", " ", " ", " "),
+            String.join("", "*", " ", " ", " ", " "),
+            String.join("", " ", " ", " ", " ", " "),
+            String.join("", " ", " ", " ", " ", " ")
+        };
+    }
+
+    // Helper method for letter S
+    static String[] buildS() {
+        return new String[] {
+            String.join("", "*", "*", "*", "*", "*"),
+            String.join("", "*", " ", " ", " ", " "),
+            String.join("", "*", "*", "*", "*", "*"),
+            String.join("", " ", " ", " ", " ", "*"),
+            String.join("", "*", "*", "*", "*", "*"),
+            String.join("", " ", " ", " ", " ", " "),
+            String.join("", " ", " ", " ", " ", " ")
+        };
+    }
+
     public static void main(String[] args) {
-        System.out.println("UC6 Implementation");
+
+        String[] O = buildO();
+        String[] P = buildP();
+        String[] S = buildS();
+
+        // Render OOPS banner
+        for (int i = 0; i < O.length; i++) {
+            System.out.println(O[i] + "  " + O[i] + "  " + P[i] + "  " + S[i]);
+        }
     }
 }
